77ce2e2cd77c2bff0653a76a7f1a72129ad62af4
Mikhail Aralin <mikhail.aralin@indusoft.ru>
2023-05-10 10:32:16 +0300

    3-module completed

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,8 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumSalary = 0;
+  for (let key in salaries){
+    if (typeof salaries[key] === "number" && isFinite(salaries[key])){
+      sumSalary += salaries[key];
+    } 
+  } return sumSalary;
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,8 @@
 function isEmpty(obj) {
-  // ваш код...
+  if (Object.keys(obj).length) {
+    return false;
+  } else {
+    return true;
+  }
 }
+
